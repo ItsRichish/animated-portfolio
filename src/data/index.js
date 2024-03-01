7b262fbb85c507efddd4507a86202933df830f17
Richish Mandal <richishm19@gmail.com>
2024-03-02 00:04:09 +0530

    Initial Commit

---
+++ src/data/index.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/wBV6qBGw/ss.png",
+    title: "Imaginify - Image Modifying App",
+    desc: "",
+    link: "https://richi-imaginify.vercel.app/",
+    git: "https://github.com/ItsRichish/imaginify",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/YCbwmPt3/ss2.png",
+    title: "Cinemate - Movie App",
+    desc: "",
+    link: "https://cinemate-richi.netlify.app/",
+    git: "https://github.com/ItsRichish/Cinemate",
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/5y3skZ7K/ss3.png",
+    title: "Trap - Music Player",
+    desc: "",
+    link: "https://trap-richi.netlify.app/",
+    git: "https://github.com/ItsRichish/Trap",
+  },
+];
